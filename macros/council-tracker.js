58d5f7a1c3e424e8e053a3ac711c5c38f13b3f4d
cineasteroid <elidcantrell@gmail.com>
2022-10-04 13:53:37 -0400

    fixed formatting inconsistencies

--- macros/council-tracker.js
+++ macros/council-tracker.js
@@ -1,79 +1,79 @@
 //The content of the Dialog box
 const content = `
-    <form>
-        <h2>Council Success Tracker</h2>
-        <p class='instructions'>Retrieve data from the most recently-created Council by clicking the button below:</p> 
-        <div class='tracker'>
-            <button id="retrieve" name="retrieve">Retrieve Council Data from Chat</button>
-            <p class='info' id="p1"></p>
-            <button id="success" class="results" name="success">Add Success</button>
-            <button id="fail" class="results" name="fail">Add Fail</button>
-            <p class='final' id="closeText"></p>
-        </div>
-    </form>
+  <form>
+    <h2>Council Success Tracker</h2>
+    <p class='instructions'>Retrieve data from the most recently-created Council by clicking the button below:</p> 
+    <div class='tracker'>
+      <button id="retrieve" name="retrieve">Retrieve Council Data from Chat</button>
+      <p class='info' id="p1"></p>
+      <button id="success" class="results" name="success">Add Success</button>
+      <button id="fail" class="results" name="fail">Add Fail</button>
+      <p class='final' id="closeText"></p>
+    </div>
+  </form>
 `;
 
 //CSS Styling for Dialog box content
 const style = `
-    <style>
-        .dialog.window-app > .window-content {
-		    background: url(ui/parchment.jpg);
-		}
-		.window-app {
-            height: 365px;
-            width: auto;
-        }
-        .dialog form {
-            margin: 10px 4px;
-        }
-        #success {
-            visibility: hidden;
-        }
-        #fail {
-            visibility: hidden;
-        }
-        button {
-            cursor: pointer;
-        }
-        .tracker {
-            text-align: center;
-        }
-        #retrieve {
-            width: 300px;
-        }
-        .dialog button:active {
-            background: rgba(0, 0, 0, 0.2);
-        }
-        .results {
-            width: 150px;
-            display: inline;
-        }
-        #outcome {
-            visibility: hidden;
-        }
-        .dialog .tracker {
-            margin: 10px 4px;
-        }
-        .dialog h2 {
-            text-align: center;
-            font-size: 1.5rem;
-        }
-        p.instructions {
-            font-size: 1rem;
-            color: #333;
-        }
-        p.final {
-            font-size: .75rem;
-            color: #333;
-            text-align: center;
-            margin-top: 48px;
-        }
-        p.info {
-            font-size: 1.15rem;
-            text-align: center;
-            font-weight: bold;
-        }
-    </style>
+  <style>
+    .dialog.window-app > .window-content {
+      background: url(ui/parchment.jpg);
+    }
+    .window-app {
+      height: 365px;
+      width: auto;
+    }
+    .dialog form {
+      margin: 10px 4px;
+  }
+    #success {
+      visibility: hidden;
+    }
+    #fail {
+      visibility: hidden;
+  }
+    button {
+      cursor: pointer;
+    }
+    .tracker {
+      text-align: center;
+    }
+    #retrieve {
+      width: 300px;
+    }
+    .dialog button:active {
+      background: rgba(0, 0, 0, 0.2);
+    }
+    .results {
+      width: 150px;
+      display: inline;
+    }
+    #outcome {
+      visibility: hidden;
+    }
+    .dialog .tracker {
+      margin: 10px 4px;
+    }
+    .dialog h2 {
+      text-align: center;
+      font-size: 1.5rem;
+    }
+    p.instructions {
+      font-size: 1rem;
+      color: #333;
+    }
+    p.final {
+      font-size: .75rem;
+      color: #333;
+      text-align: center;
+      margin-top: 48px;
+    }
+    p.info {
+      font-size: 1.15rem;
+      text-align: center;
+      font-weight: bold;
+    }
+  </style>
 `;
 
 const SET_VISIBILITY_VISIBLE = 'visible';
@@ -95,7 +95,7 @@ function getElements() {
   return { DATA_BTN, SUCC_BTN, FAIL_BTN, P1_TEXT, CLOSE_TEXT };
 }
 
-//Retrieve the last Council message and return the needed values within
+//Retrieve the most recent Council message and return the values for Resistance (Successes needed) and Time Limit
 function getCouncil() {
   const msgs = game.messages.contents;
   const lastCouncil = [...msgs]
@@ -120,7 +120,7 @@ function getCouncil() {
   }
 }
 
-//Adds additional text to the Dialog content containing the pertinent Council parameters
+//Add additional text to the Dialog content containing the pertinent Council parameters
 function addParameters() {
   const elem = getElements();
   const succBtn = elem.SUCC_BTN;
@@ -131,9 +131,9 @@ function addParameters() {
   failBtn.style.visibility = SET_VISIBILITY_VISIBLE;
 }
 
-//When Add Success/Add Failure buttons are pressed, outputs a message to chat that tallies each
+//When Add Success/Add Failure buttons are pressed, output a message to chat that tallies each
 function addSuccess() {
-  ++successTally;
+  successTally++;
   let chatData = {
     speaker: ChatMessage.getSpeaker({
       alias: `Council with ${council.audience}`,
@@ -143,14 +143,12 @@ function addSuccess() {
       'fail-tally': failTally,
     },
     content: `
-                <h2 style="text-align: center; color: #33803c;">Success!</h2>
-                <p><strong>Successes: </strong>${successTally}/${resistVal}</p>
-                <p><strong>Failures: </strong>${failTally}</p>
-                <hr>
-                <p><strong>Attempt: </strong>${
-                  successTally + +failTally
-                }/${timeLimit}</p>
-        `,
+      <h2 style="text-align: center; color: #33803c;">Success!</h2>
+      <p><strong>Successes: </strong>${successTally}/${resistVal}</p>
+      <p><strong>Failures: </strong>${failTally}</p>
+      <hr>
+      <p><strong>Attempt: </strong>${successTally + +failTally}/${timeLimit}</p>
+    `,
   };
 
   let msg = ChatMessage.create(chatData, {});
@@ -159,7 +157,7 @@ function addSuccess() {
 }
 
 function addFailure() {
-  ++failTally;
+  failTally++;
   let chatData = {
     speaker: ChatMessage.getSpeaker({
       alias: `Council with ${council.audience}`,
@@ -169,14 +167,12 @@ function addFailure() {
       'fail-tally': failTally,
     },
     content: `
-                <h2 style="text-align: center; color: #780c0c;">Fail!</h2>
-                <p><strong>Successes: </strong>${successTally}/${resistVal}</p>
-                <p><strong>Failures: </strong>${failTally}</p>
-                <hr>
-                <p><strong>Attempt: </strong>${
-                  successTally + +failTally
-                }/${timeLimit}</p>
-        `,
+      <h2 style="text-align: center; color: #780c0c;">Fail!</h2>
+      <p><strong>Successes: </strong>${successTally}/${resistVal}</p>
+      <p><strong>Failures: </strong>${failTally}</p>
+      <hr>
+      <p><strong>Attempt: </strong>${successTally + +failTally}/${timeLimit}</p>
+    `,
   };
 
   let msg = ChatMessage.create(chatData, {});
@@ -193,9 +189,9 @@ function checkTally() {
         alias: `Council with ${council.audience}`,
       }),
       content: `
-                    <h2 style="text-align: center; color: #33803c;">Council Success</h2>
-                    <p>You have succeeded in your Council with ${council.audience}!</p>
-            `,
+          <h2 style="text-align: center; color: #33803c;">Council Success</h2>
+          <p>You have succeeded in your Council with ${council.audience}!</p>
+        `,
     };
 
     let msg = ChatMessage.create(chatData, {});
@@ -217,16 +213,16 @@ function checkTally() {
   } else if (
     (timeLimit - (successTally + failTally) < resistVal - successTally &&
       successTally < resistVal) ||
-    (sucessTally + failTally === timeLimit && successTally < resistVal)
+    (successTally + failTally === timeLimit && successTally < resistVal)
   ) {
     let chatData = {
       speaker: ChatMessage.getSpeaker({
         alias: `Council with ${council.audience}`,
       }),
       content: `
-                    <h2 style="text-align: center; color: #780c0c;">Council Failure</h2>
-                    <p>You have failed in your Council with ${council.audience}.</p>
-            `,
+          <h2 style="text-align: center; color: #780c0c;">Council Failure</h2>
+          <p>You have failed in your Council with ${council.audience}.</p>
+        `,
     };
 
     let msg = ChatMessage.create(chatData, {});
